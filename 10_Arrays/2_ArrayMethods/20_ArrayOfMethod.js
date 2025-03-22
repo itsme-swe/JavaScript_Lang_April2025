abd037d4e0dc5b50efbef152c10bbb343d7d64c2
itsme-swe <me.harshmehra07@gmail.com>
2025-03-22 09:30:01 +0530

    update

---
+++ 10_Arrays/2_ArrayMethods/20_ArrayOfMethod.js
@@ -0,0 +1,13 @@
+/*
+💥 The Javascript array.of() method is an inbuilt method in JavaScript that creates a new array instance with variables present as the argument of the method.
+*/
+
+let sc1 = 20;
+let sc2 = 30;
+let sc3 = 23;
+let sc4 = 10;
+let sc5 = 50;
+
+let totalScore = Array.of(sc1, sc2, sc3, sc4, sc5);
+
+console.log(totalScore); // [ 20, 30, 23, 10, 50 ]
