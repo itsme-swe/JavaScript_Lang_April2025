5e3b6a55e2ba4c2a98adb42a399b5261c259a4ab
itsme-swe <me.harshmehra07@gmail.com>
2025-03-22 09:02:15 +0530

    update

--- 10_Arrays/2_ArrayMethods/1_concat.js
+++ 10_Arrays/2_ArrayMethods/1_concat.js
@@ -7,5 +7,18 @@ let c = a.concat(b);
 console.log(c); // [ 2, 6, 8, 1, 3, 7 ]
 
 /*
-💥 The concat() method of Array instances is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+💥 The concat() method of Array instances is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array. 
+💥 And, even we can use spread operator to merge the arrays.
 */
+
+console.log();
+
+let arr = [2, 4, 6, 8];
+
+let str = ["Harsh", "Juhu", "Vandana", "Gaurav"];
+
+let cars = ["Roxx", "i20", "Nexon"];
+
+let newArr = [...arr, ...str, ...cars];
+
+console.log(newArr);
