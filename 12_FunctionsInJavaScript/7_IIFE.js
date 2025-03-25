d2961a8c1188207a2688b51c753f7ae3893d4ab5
itsme-swe <me.harshmehra07@gmail.com>
2025-03-25 17:14:36 +0530

    update

---
+++ 12_FunctionsInJavaScript/7_IIFE.js
@@ -0,0 +1,43 @@
+/*
+💥 Immediately Invoked Function Expressions (IIFE) are JavaScript functions that are executed immediately after they are defined. They are typically used to create a local scope for variables to prevent them from polluting the global scope.
+
+IIFEs are commonly used to create private scope in JavaScript, allowing variables and functions to be encapsulated and inaccessible from outside the function.
+*/
+
+(function dbConnection() {
+  console.log(`DB connected`);
+})();
+
+// DB Connected
+
+console.log();
+
+const result = (() => {
+  let a = 10;
+  let b = 20;
+  return a + b;
+})();
+
+console.log(result); // 30
+
+console.log();
+
+/*
+💥 IIFEs are commonly used to create private scope in JavaScript, allowing variables and functions to be encapsulated and inaccessible from outside the function.
+*/
+const counter = (() => {
+  var count = 0;
+
+  return {
+    increment: () => count++,
+    decrement: () => count--,
+    getCount: () => count,
+  };
+})();
+
+counter.increment();
+counter.increment();
+
+console.log(counter.getCount()); // 2
+
+console.log(counter.count); // Output: undefined (cannot access private variable)
