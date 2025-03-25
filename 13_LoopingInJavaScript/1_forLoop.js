de54058752eded69225a86acba811f3308a10934
itsme-swe <me.harshmehra07@gmail.com>
2025-03-25 18:43:37 +0530

    update

---
+++ 13_LoopingInJavaScript/1_forLoop.js
@@ -0,0 +1,16 @@
+for (let i = 1; i <= 10; i++) {
+  const element = i;
+  process.stdout.write(element + " ");
+}
+
+// 1 2 3 4 5 6 7 8 9 10
+
+console.log();
+
+let arr = [2, 4, 6, 8];
+
+for (let i = 0; i < arr.length; i++) {
+  process.stdout.write(arr[i] + " ");
+}
+
+// 2 4 6 8
