f5794e56efce86ff04f9f86f6177ecad7022ddbc
itsme-swe <me.harshmehra07@gmail.com>
2025-03-25 21:27:13 +0530

    update

--- 13_LoopingInJavaScript/HigherOrderLoops/3_forEachLoop.js
+++ 13_LoopingInJavaScript/HigherOrderLoops/3_forEachLoop.js
@@ -1,2 +1,23 @@
 let arr = ["Java", "Python", "Swift", "Go", "Rust"];
 
+arr.forEach(function (val) {
+  console.log(val);
+});
+
+console.log();
+
+// 2️⃣ Arrow Function
+
+arr.forEach((ele) => {
+  console.log(ele);
+});
+
+console.log();
+
+// 3️⃣ Passing another function as parameter in forEach Loop
+
+function printElements(items) {
+  console.log(items);
+}
+
+arr.forEach(printElements); // Here we are just passing reference of the function
