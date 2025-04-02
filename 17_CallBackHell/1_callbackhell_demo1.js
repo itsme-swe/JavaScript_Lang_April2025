fa43db541a3c785b9c9c7070a199b2afdeca2293
itsme-swe <me.harshmehra07@gmail.com>
2025-04-02 19:37:41 +0530

    update

--- 17_CallBackHell/1_callbackhell_demo1.js
+++ 17_CallBackHell/1_callbackhell_demo1.js
@@ -1,17 +1,17 @@
 /*
-Here we are writing steps how we placed the order on ecom website.
-1️⃣ We add items in cart.
-2️⃣ Then we create order.
-3️⃣ And, then we proceed to payment
+ Order Placement Steps on an E-commerce Website:
+1️⃣ Add items to the cart.
+2️⃣ Create an order.
+3️⃣ And, then Proceed to payment.
 
-So to achieve these steps callback hell comes in picture.
+To implement these steps, we use callbacks, which can lead to:
 
 💥 Callback hell -- "Callback hell," also known as the "pyramid of doom," refers to a situation in programming where multiple nested callbacks are used to handle asynchronous operations, making the code difficult to read, understand, and maintain. 
 */
 
 const cart = ["shoes", "t-shirt", "watch", "laptop bag"]; // ⇨ items added to the cart
 
-// Here 'makePayment' is the callback function
+// Here, 'makePayment' is a callback function used within 'createOrder'
 api.createOrder(cart, function () {
   api.makePayment(function () {
     api.showOrderSummary(function () {
