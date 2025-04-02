3a6a09f8c039036873da4cb48540f2980b995c1f
itsme-swe <me.harshmehra07@gmail.com>
2025-04-02 19:55:15 +0530

    update

---
+++ 16_Promise/6_promiseDemo_1.js
@@ -0,0 +1,14 @@
+const cart = ["shoes", "t-shirt", "watch", "laptop bag"]; // ⇨ items added to the cart
+
+// 1️⃣ Handling asynchronous operations using callbacks (before Promises were introduced)
+createOrder(cart, function (orderId) {
+  makePayment(orderId);
+});
+
+// 2️⃣ Handling asynchronous operations using Promises (modern approach)
+const promise = createOrder(cart); // ⇨ 'createOrder' now returns a Promise
+
+// Earlier we were passing callback function to createOrder() function, but here we are attaching callback function to the promise object.
+promise.then(function (orderId) {
+  makePayment(orderId);
+});
