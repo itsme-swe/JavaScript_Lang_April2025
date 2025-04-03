a04e7d9bea53f8f1ca6c911c73f51189c2643392
itsme-swe <me.harshmehra07@gmail.com>
2025-04-03 08:38:28 +0530

    update

--- 18_FetchAPI/1_fetchAPI_Deno1.js
+++ 18_FetchAPI/1_fetchAPI_Deno1.js
@@ -1,9 +1,9 @@
 const GITHUB_API = "https://api.github.com/users/harshmehra";
 
-console.log(typeof GITHUB_API); // output ⇨ string
+console.log(typeof GITHUB_API); // ◽output ⇨ string
 
 const user = fetch(GITHUB_API);
 
-console.log(user); // ◽ output:  The fetch() returns an promise object ⇨ Promise { <pending> }
+console.log(user); // ◽output:  The fetch() returns an promise object ⇨ Promise { <pending> }
 
-console.log(typeof user); // ◽ output: object
+console.log(typeof user); // ◽output: object
