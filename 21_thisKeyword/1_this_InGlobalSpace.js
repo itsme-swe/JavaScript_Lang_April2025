27666170d774b829fb170388c22bba54d63627a4
itsme-swe <me.harshmehra07@gmail.com>
2025-04-04 21:19:18 +0530

    update

---
+++ 21_thisKeyword/1_this_InGlobalSpace.js
@@ -0,0 +1,5 @@
+"use strict";
+
+console.log(this);
+
+// ◽ output: Window {0: global, window: Window, self: Window, document: document, name: '', location: Location, …}  ⇨ But this is the global space of browser
